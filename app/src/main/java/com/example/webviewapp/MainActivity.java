d1c07a322dc019337b4867276c45ca9fed7de12d
Novin <a20novra@student.his.se>
2021-04-09 16:55:50 +0200

    Implement "Show externa and internal page"

--- app/src/main/java/com/example/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.webviewapp;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -7,6 +8,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -18,15 +20,24 @@ import androidx.appcompat.widget.Toolbar;
 public class MainActivity extends AppCompatActivity {
 
     private WebView myWebView;
+    private WebViewClient myWebviewClient;
 
     public void showExternalWebPage(){
         // TODO: Add your code for showing external web page here
+        //Implement showExternalWebPage() and showInternalWebPage(). Hint: loadUrl().
+        WebView webView = new WebView(this); // external
+        myWebView.loadUrl("https://his.se"); //external
     }
 
     public void showInternalWebPage(){
         // TODO: Add your code for showing internal web page here
+        WebViewClient myWebViewClient = new WebViewClient();
+        myWebView.setWebViewClient(myWebviewClient); //internal
+        myWebView.loadUrl("file:///android_asset/duggaindex.html");
     }
 
+
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,39 +45,12 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
-
-        WebView myWebView = (WebView) findViewById(R.id.my_webview);
-        myWebView.loadUrl("https://his.se");
+        myWebView = (WebView) findViewById(R.id.my_webview);
 
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
 
-        /*
-
-        * Rename your App. Tip: Values->Strings
-        * Enable Internet access for your App. Tip: Manifest
-        * Create a WebView element in the layout file content_main.xml
-        * Give the WebView element ID "my_webview"
-        -- Commit and push to your github fork
-        * Create a private member variable called "myWebView" of type WebView
-        * Locate the WebView element created in step 1 using the ID created in step 2
-        * Create a new WebViewClient to attach to our WebView. This allows us to
-          browse the web inside our app.
-        -- Commit and push to your github fork
-        * Enable Javascript execution in your WebViewClient
-        * Enter the url to load in our WebView
-        -- Commit and push to your github fork
-        * Move the code that loads a URL into your WebView into the two methods
-          "showExternalWebPage()" and "showInternalWebPage()".
-        * Call the "showExternalWebPage()" / "showInternalWebPage()" methods
-          when you select menu options "External Web Page" or "Internal Web Page"
-          respectively
-        -- Commit and push to your github fork
-        * Take two screenshots using the "Take a screenshot" tool in the AVD
-           showing your App. One (1) screenshot showing your internal web page and
-           one (1) screenshot showing your external web page.
-
+ /*
         WebView myWebView = new WebView( this);
         setContentView(myWebView);
         myWebView.loadUrl("https://his.se");
@@ -90,16 +74,16 @@ public class MainActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_external_web) {
             Log.d("==>","Will display external web page");
+            showExternalWebPage();
             return true;
         }
 
         if (id == R.id.action_internal_web) {
             Log.d("==>","Will display internal web page");
+            showInternalWebPage();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
-
-
 }
